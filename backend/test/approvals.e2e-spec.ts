1c763b5a8ae3c899af64f58915e0914fe9dd4502
Du Yanchao <du_yanchao@tech.gov.sg>
2025-10-24 22:15:45 +0800

    fix cypress tests

--- backend/test/approvals.e2e-spec.ts
+++ backend/test/approvals.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('Approvals (e2e)', () => {
